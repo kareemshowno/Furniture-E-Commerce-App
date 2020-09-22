fef526369344c0d97904d9bfd0cb6c25d9742814
kareemshowno <65797037+kareemshowno@users.noreply.github.com>
2020-09-22 21:39:50 +0300

    Updates

---
+++ precache-manifest.0e6eeb57772ed591f77245f4004b188c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7e3dff5fd287ccbb1212f6134e7eba",
+    "url": "/Furniture-E-Commerce-App/index.html"
+  },
+  {
+    "revision": "c863fd5f681d696ed718",
+    "url": "/Furniture-E-Commerce-App/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "72b27453a4881fca85e2",
+    "url": "/Furniture-E-Commerce-App/static/css/main.91e8f9e4.chunk.css"
+  },
+  {
+    "revision": "c863fd5f681d696ed718",
+    "url": "/Furniture-E-Commerce-App/static/js/2.8c275ad1.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/Furniture-E-Commerce-App/static/js/2.8c275ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b27453a4881fca85e2",
+    "url": "/Furniture-E-Commerce-App/static/js/main.0f4fa916.chunk.js"
+  },
+  {
+    "revision": "7fe2b18491d5f3d621d9",
+    "url": "/Furniture-E-Commerce-App/static/js/runtime-main.111ab755.js"
+  },
+  {
+    "revision": "a3575759363114ddc62c9d0b16b36b5c",
+    "url": "/Furniture-E-Commerce-App/static/media/banner-home.a3575759.jpg"
+  }
+]);
